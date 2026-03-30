ad9983184607c1f7a3c6b0bf979e7c482df40f39
Alma <codingskills198@gmail.com>
2026-03-30 01:01:10 -0400

    lesson-01 about me exercise

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,27 @@
 
 export default function StudentWork() {
   //add variables here
+  const myName = 'Alma';
+  const age = 31;
+  const hobbies = ['Drawing', 'Traveling', 'Reading'];
+
   return (
     <div>
       {/* add JSX here */}
-      <p> Student output will go here </p>
+      <h1>About me</h1>
+      <p>
+        {' '}
+        Hi! I am {myName} and I am {age} years old. I am currently learning
+        React and really enjoying it. I already have experience with HTML, CSS,
+        and JavaScript and I am excited to add React to my skill set. I am
+        looking forward to growing as a developer!{' '}
+      </p>
+      <p>My hobbies are:</p>
+      <ul>
+        {hobbies.map((hobby, index) => (
+          <li key={index}>{hobby}</li>
+        ))}
+      </ul>
     </div>
   );
 }
